ad3640287238ea94c70aa86b4206164c973d0d43
Nicolas Castro <nicolas.y0909@gmail.com>
2024-06-08 21:13:45 -0300

    refactor(front/src/@core/main/factories/socket-client/socket-client-factory): alterando a url do socket io server.

--- front/src/@core/main/factories/socket-client/socket-client-factory.ts
+++ front/src/@core/main/factories/socket-client/socket-client-factory.ts
@@ -2,5 +2,5 @@ import { SocketClientBuilder } from "~/@core/infra";
 
 export const makeSocketClient = (): SocketClientBuilder => {
 
-    return new SocketClientBuilder("zap-2.vercel.app");
+    return new SocketClientBuilder("152.67.63.68:3001");
 } 
